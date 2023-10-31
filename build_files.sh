eeed4b4638de061b37b5d5a2194af10d4de6c39f
atul120212 <atulsharma28092004@gmail.com>
2023-10-31 23:26:54 +0530

    update8

--- build_files.sh
+++ build_files.sh
@@ -1,13 +1,5 @@
- echo "BUILD START"
- # python3.9 -m pip install -r requirements.txt
- python3.9 -m venv venv
-    source venv/bin/activate
- pip install -r requirements.txt
- # python3.9 manage.py collectstatic
- # ls
- echo "BUILD END"
-# source venv/bin/activate
 
- # Collect static files
-echo "Collecting static files..."
-python manage.py collectstatic
+echo "Build files for learning"
+python3.9 -m pip install -r requirements.txt -f
+python3.9 manage.py collectstatic
+echo "Build end"
